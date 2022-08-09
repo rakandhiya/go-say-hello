5ed3655a070d9a19b28fa97e87cd4cf4137f70dd
Rakandhiya <rakandhiya.daanii@gmail.com>
2022-08-09 21:41:09 +0700

    breaking changes

--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,7 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello World!"
+func SayHello(name string) string {
+	return "Hello World from " + name + "!"
 }
 
 func SayHelloToName(name string) string {
